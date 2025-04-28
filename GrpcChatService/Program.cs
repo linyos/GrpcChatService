9c83d0924340f01d4693b71e8654a1fa975d3c14
linyos <laso85143@gmail.com>
2025-04-28 23:15:08 +0800

    Add initial implementation of GrpcChatService with multiple components and layouts
    - Created App.razor for routing and layout management.
    - Added MainLayout.razor for the main application layout.
    - Implemented NavMenu.razor for navigation links.
    - Developed various pages including Home, Clients, and Index with relevant content.
    - Set up _Host.cshtml and _Layout.cshtml for HTML structure and styling.
    - Included app.css for custom styles and Bootstrap integration.
    - Established routing in Component1.razor and Routes.razor.
    - Implemented error handling UI in index.html and _Layout.cshtml.
    - Added necessary imports in _Imports.razor for component accessibility.

--- GrpcChatService/Program.cs
+++ GrpcChatService/Program.cs
@@ -1,4 +1,3 @@
-
 // �s�WLOG
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
@@ -8,26 +7,28 @@ Log.Logger = new LoggerConfiguration()
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 //  Serilog  Logger
 builder.Host.UseSerilog();
 
 // Add services to the container.
 builder.Services.AddGrpc();
-builder.Services.AddRazorPages(); 
+builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<ChatState>();
 
-
 var app = builder.Build();
-
-
-
-
-
-app.MapGrpcService<GreeterService>();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 app.MapBlazorHub();
+app.MapGrpcService<GreeterService>();
+
+// 這裡要指向 Pages 資料夾下的 _Host
 app.MapFallbackToPage("/_Host");
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
